599fd326f8e9dbe215951624817d5ce5429e9373
TehranMammadov <tehranmammadov.dev.01@gmail.com>
2025-01-10 11:29:33 +0400

    local url added to replace base url

--- client/src/utils/replaceurl.js
+++ client/src/utils/replaceurl.js
@@ -1,9 +1,12 @@
 function replaceBaseURL(url) {
-  if (url) return url.replace(
+  if (url && url.includes(process.env.REACT_APP_URL_LOCAL)) return url.replace(
+    process.env.REACT_APP_URL_LOCAL,
+    process.env.REACT_APP_URL
+  )
+  return url.replace(
     process.env.REACT_APP_URL_PROD,
     process.env.REACT_APP_URL
   )
-  return
 }
 
 export default replaceBaseURL;
