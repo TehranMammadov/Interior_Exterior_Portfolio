ed70322301ea8f4bcea8b29c8536a258cc3a5e73
TehranMammadov <tehranmammadov.dev.01@gmail.com>
2025-01-10 11:34:54 +0400

    replace bug fixed

--- client/src/utils/replaceurl.js
+++ client/src/utils/replaceurl.js
@@ -2,11 +2,14 @@ function replaceBaseURL(url) {
   if (url && url.includes(process.env.REACT_APP_URL_LOCAL)) return url.replace(
     process.env.REACT_APP_URL_LOCAL,
     process.env.REACT_APP_URL
-  )
-  return url.replace(
+  ) 
+  
+  else if (url) return url.replace(
     process.env.REACT_APP_URL_PROD,
     process.env.REACT_APP_URL
   )
+
+  return
 }
 
 export default replaceBaseURL;
