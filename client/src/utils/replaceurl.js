8e31572b37ab96c965bdf9f80c064d4db12dd749
TehranMammadov <tehranmammadov.dev.01@gmail.com>
2025-01-10 11:38:04 +0400

    image url debug console

--- client/src/utils/replaceurl.js
+++ client/src/utils/replaceurl.js
@@ -1,9 +1,10 @@
 function replaceBaseURL(url) {
+  console.log(url, url.includes(process.env.REACT_APP_URL_LOCAL))
   if (url && url.includes(process.env.REACT_APP_URL_LOCAL)) return url.replace(
     process.env.REACT_APP_URL_LOCAL,
     process.env.REACT_APP_URL
   ) 
-  
+
   else if (url) return url.replace(
     process.env.REACT_APP_URL_PROD,
     process.env.REACT_APP_URL
